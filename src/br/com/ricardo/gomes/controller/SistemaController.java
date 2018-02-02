03ed77db76ee880a292a42a96f5a6257c99d2084
Ricardo Julio de Jesus Gomes <ricardojj.gomes.SPASSU@petrobras.com.br>
2018-02-02 16:15:00 -0200

    Commit inicial

---
+++ src/br/com/ricardo/gomes/controller/SistemaController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package br.com.ricardo.gomes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/sistema")
+public class SistemaController {
+	
+	@RequestMapping(value = "/boasvindas", method = RequestMethod.GET)
+	public ModelAndView boasVindas(@RequestParam(value = "nome", required = false, defaultValue = "Ricardo Gomes") String nome) {
+
+		ModelAndView modelAndView = new ModelAndView("boasVindas");
+		modelAndView.addObject("mensagem", "Olá " + nome);
+
+		return modelAndView;
+	}
+
+}
